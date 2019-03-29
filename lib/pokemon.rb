426b63a8605a7bd828c8b8ed7722d94802f9cf02
Brian Simonton <bsimonton78@gmail.com>
2019-03-29 19:10:16 +0000

    Automatically backed up by Learn

--- lib/pokemon.rb
+++ lib/pokemon.rb
@@ -1,7 +1,7 @@
 class Pokemon
  attr_accessor :name, :type, :db, :id, :hp
 
-  def initialize(id, name, type, db)
+  def initialize(keywords)
 
   end
 
@@ -19,6 +19,10 @@ class Pokemon
     return new_pokemon
   end
 
+  def alter_hp(num, db)
+    db.execute("UPDATE pokemon SET hp = ? WHERE id = ?", [num], [self.id])
+    self.hp = num
+  end
   
   
   
